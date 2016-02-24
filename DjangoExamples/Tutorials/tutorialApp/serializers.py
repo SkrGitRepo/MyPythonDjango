15f08c8d6861639e6634a1d1d49800b181b3b7c6
SkrGitRepo <sumkuma2@SUMKUMA2-98EN8.partnet.cisco.com>
2016-02-24 12:48:24 +0530

    This One of the DJango-Rest Framework Example

---
+++ DjangoExamples/Tutorials/tutorialApp/serializers.py
@@ -0,0 +1,18 @@
+'''
+Created on Oct 29, 2015
+
+@author: sumkuma2
+'''
+from django.contrib.auth.models import User, Group
+from rest_framework import  serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url','username','email','groups')
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url','name')
+        
