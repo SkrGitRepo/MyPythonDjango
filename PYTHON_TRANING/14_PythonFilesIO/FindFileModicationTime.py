56584d12f8f115c6f4911bd656c703a05199a46a
Sumit Kumar <skr2getmail@gmail.com>
2016-03-30 17:01:28 +0530

    Added some more line of os.path example

--- PYTHON_TRANING/14_PythonFilesIO/FindFileModicationTime.py
+++ PYTHON_TRANING/14_PythonFilesIO/FindFileModicationTime.py
@@ -9,3 +9,11 @@ import time
 file="readme.txt"
 print "last modified: %s" % time.ctime(os.path.getmtime(file))
 print "created: %s" % time.ctime(os.path.getctime(file))
+print "-----------------------------------------------------------------------------"
+print os.__all__
+print "-----------------------------------------------------------------------------"
+print os.path.__all__
+print os.path.__doc__
+print "-----------------------------------------------------------------------------"
+print time.__doc__
+print "-----------------------------------------------------------------------------"
