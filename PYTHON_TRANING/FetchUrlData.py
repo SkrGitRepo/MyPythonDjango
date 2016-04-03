5137a1fdfbe87106ae0e9b6f0db32390acd6aef6
Sumit Kumar <skr2getmail@gmail.com>
2016-04-03 09:33:10 +0530

    Added new url to fetch data

--- PYTHON_TRANING/FetchUrlData.py
+++ PYTHON_TRANING/FetchUrlData.py
@@ -5,9 +5,10 @@ Created on Feb 20, 2016
 '''
 
 import urllib2
+import re
 
-
-response = urllib2.urlopen('http://mailer-api.cisco.com/itsm/mailer/rest/text/noauth/members/cpo-dev-superuser')
+#response = urllib2.urlopen('http://mailer-api.cisco.com/itsm/mailer/rest/text/noauth/members/cpo-dev-superuser')
+response = urllib2.urlopen('http://127.0.0.1:8090/prime/optical/onramp/api/usertype/cpo-dev-superuser')
 datas = response.read()
 
 mydatadict = {'datadict':datas}
@@ -16,6 +17,15 @@ mydatadict = {'datadict':datas}
 users = datas.split('\n')
 users = filter(None,users) #to remove any empty list item
 print users;
+
+user_list = []
+for user in users:
+    user = re.sub('\t|<br/>','', user.strip())
+    #user = re.sub('<br/>','', user.strip())
+    #user = re.sub('\n','', user.strip())
+    user_list.append(user)
+user_list = filter(None,user_list) #to remove any empty list item    
+print(user_list)
 
 if 'alchu' in users:
     print "match found"
